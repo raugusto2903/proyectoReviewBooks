c0e8a7786cb570ea7748593c0275ffb13f538694
raugusto2903 <ramirol@titamedia.com>
2024-11-04 16:25:57 -0500

    Configuracion cloud en google

---
+++ ReviewBooks/Data/Migrations/20241104162003_FinalProofCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewBooks.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalProofCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
